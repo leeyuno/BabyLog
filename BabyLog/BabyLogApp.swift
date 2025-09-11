4929bdb086fbb22a824163a1a0b4e9c870b4e7ce
leeyuno <leeyuno@naver.com>
2025-09-11 21:30:18 +0900

    Initial Commit

---
+++ BabyLog/BabyLogApp.swift
@@ -0,0 +1,20 @@
+//
+//  BabyLogApp.swift
+//  BabyLog
+//
+//  Created by 이윤오 on 2025/09/11.
+//
+
+import SwiftUI
+
+@main
+struct BabyLogApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
